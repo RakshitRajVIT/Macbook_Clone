95cf0a94d753c1a824d8fd26f4c867c3da280f74
RakshitRajVIT <rakshitraj2323@gmail.com>
2026-01-04 17:34:24 +0530

    Added Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/all";
+import ShowCase from './components/ShowCase'
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const App = () => {
   return (
@@ -9,6 +15,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <ProductViewer/>
+        <ShowCase/>
     </main>
   )
 }
